a1a51e73e7d32d86f1e593707edd0bc890a7f875
TPrevoteau <titouan.prevoteau@yahoo.fr>
2021-05-19 23:20:28 +0200

    Separated first components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import './App.css';
+import Header from './components/header.js';
+import Map from './components/map.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+      <Header />
+      <Map />
     </div>
   );
 }
---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+function Header() {
+    return (
+      <div className="App-header">
+        <header>
+          <p>
+            Pech'eau
+          </p>
+        </header>
+      </div>
+    );
+  }
+  
+  export default Header;
