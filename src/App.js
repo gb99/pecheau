866d4b0475b6f9f0c3128e76989b8d9ddd12c6b7
dylan-correia <79135837+dylan-correia@users.noreply.github.com>
2021-06-03 08:29:36 +0200

    Hubeau init (#1)
    * Initialisation de la map et récupération des données hub'eau
    
    * Lien avec DynamoDB
    
    - Jeu de test dans DynamoDB
    - Lien entre le react et DynamoDB
    
    * Appel à hub'eau + test sur les délimitations d'une région

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Hubeau from './components/hubeau.js';
+
+
+function App() {
+  return (
+    <div className="App">
+         <Hubeau/>     
+        <header className="App-header">
+        </header> 
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/hubeau.js
@@ -0,0 +1,106 @@
+import React from "react";
+import { MapContainer, TileLayer, Marker, Popup, GeoJSON } from 'react-leaflet';
+import centre from "../region/centre.json"
+
+class Hubeau extends React.Component {
+    
+    constructor(props) {
+      super(props);
+      this.state = {
+        error: null,
+        isLoaded: false,
+        items: [],
+        data: [],
+        region: []
+      };
+    }
+
+//https://mbm11j64gj.execute-api.eu-west-3.amazonaws.com/default/GetCodeStation
+    componentDidMount() {
+      fetch("https://mbm11j64gj.execute-api.eu-west-3.amazonaws.com/default/GetCodeStation")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result.Items
+          });
+        }
+      )
+    }
+
+    fishByStation(stationId){
+      console.log(stationId)
+      fetch("https://hubeau.eaufrance.fr/api/v0/etat_piscicole/poissons?code_station="+ stationId +"%2C01020102&format=json&size=20")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            data: result.data
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+    }
+
+
+    render() {
+      const { error, isLoaded, data, items } = this.state;
+      if (error) {
+        return <div>Error: {error.message}</div>;
+      } else if (!isLoaded) {
+        return <div>Loading...</div>;
+      } else {
+        return (
+         <div>
+              <MapContainer center={[49.766063546, 3.188467932]} zoom={7} scrollWheelZoom={false}>
+            
+             <TileLayer
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                           
+
+              {items.map(items => <Marker key={items.id} position={[items.y, items.x]} 
+              eventHandlers={{click: (e) => {this.fishByStation(items.stationId)}}}>
+                <Popup>                 
+                   <ul>
+                    {          
+                       data.map(data => (
+                        <li key={data.id}>
+                          {data.nom_poisson + " : " + data.effectif} 
+                        </li>
+                        ))
+                    }
+                  </ul>
+                </Popup>
+              </Marker>)
+              }
+              <GeoJSON data={centre} />
+            </MapContainer>       
+
+            <ul>
+                {          
+                    items.map(items => (
+                    <li key={items.id}>
+                     {items.region} 
+                    </li>
+                     ))
+                }
+            </ul>
+          </div>
+        );
+      }
+    }
+  }
+
+  export default Hubeau;
