b2559378d203f0f5f720b11a62958b06cfaa175b
Nosal Loïck <loick.nosal5@etu.univ-lorraine.fr>
2021-06-04 14:09:08 +0200

    implementation google-auth

--- src/config.js
+++ src/config.js
@@ -3,5 +3,8 @@ module.exports = {
         // apiKey: "275d268238364a9f865160315210206",
         apiKey: "WrongApiKeyToBeRemoved",
         apiUrl: "http://api.worldweatheronline.com/premium/v1/weather.ashx"
+    },
+    login: {
+    	clientId: "1054091480195-grpn789aa9gh48po0r915j2chmv4p0kq.apps.googleusercontent.com"
     }
 };
